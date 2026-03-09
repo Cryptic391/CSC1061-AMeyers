e258ff427148292ad718e30395697c28ecefd8ac
Meyers <S02735264@ccc.ccofc.edu>
2026-03-09 14:28:52 -0600

    3/9/26

---
+++ Recursion/src/Factorial.java
@@ -0,0 +1,18 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int num = 3;
+		System.out.println("The factorial of " + num + " is " + factorial(num));
+
+	}
+
+	private static int factorial(int n) {
+		if(n == 0 ) {
+			return 1;
+		}
+		int product = n * factorial(n-1);
+		return product;
+	}
+
+}
